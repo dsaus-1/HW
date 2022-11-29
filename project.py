09e35842e51bbc4018add305ac7391d5739e9b84
Dima Saushkin <saushkin.reg@gmail.com>
2022-11-29 16:34:02 +0300

    Initial commit

---
+++ project.py
@@ -0,0 +1,16 @@
+def solution(string):
+    st = []
+    for i in string:
+        if i != '*':
+            st.append(i)
+        else:
+            if st != []:
+                st = st[:-1]
+    if st == []:
+        return ''
+    return ''.join(st)
+
+
+
+
+
