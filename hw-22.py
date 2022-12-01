2f95767ac4480ceb8fce5c5e2e4ba9a4ff8ece57
Dima Saushkin <saushkin.reg@gmail.com>
2022-12-01 12:52:56 +0300

    'ыыыааа'

--- hw-22.py
+++ hw-22.py
@@ -1,8 +1,9 @@
 import csv
 
 
-def select_sorted(sort_columns=["high"], limit=3, group_by_name=False, order='desc', filename='dump.csv'):
+def select_sorted(sort_columns=["high"], limit=30, group_by_name=False, order='desc', filename='dump.csv'):
     list_file = []
+    sort_index = {'date': 0, 'open': 1, "high": 2, 'low': 3, 'close': 4, 'volume': 5, 'Name': 6}
     def partition(list_file, low, high, sort_columns):
         '''
         расчет индекса и замена элементов при сортировке quick_sort
@@ -10,15 +11,15 @@ def select_sorted(sort_columns=["high"], limit=3, group_by_name=False, order='de
         # Выбираем средний элемент в качестве опорного
         # Также возможен выбор первого, последнего
         # или произвольного элементов в качестве опорного
-        pivot = list_file[(low + high) // 2][sort_columns[0]]
+        pivot = list_file[(low + high) // 2][sort_index[sort_columns[0]]]
         i = low - 1
         j = high + 1
         while True:
             i += 1
-            while list_file[i][sort_columns[0]] < pivot:
+            while list_file[i][sort_index[sort_columns[0]]] < pivot:
                 i += 1
             j -= 1
-            while list_file[j][sort_columns[0]] > pivot:
+            while list_file[j][sort_index[sort_columns[0]]] > pivot:
                 j -= 1
             if i >= j:
                 return j
@@ -43,21 +44,23 @@ def select_sorted(sort_columns=["high"], limit=3, group_by_name=False, order='de
 
 
     with open(filename, encoding='utf-8') as open_file:
-        filename_reader = csv.DictReader(open_file)
-
+        filename_reader = csv.reader(open_file)
         for line in filename_reader:
             list_file.append(line)
         numline = len(open_file.readlines())
         quick_sort(list_file, sort_columns)
         if group_by_name:
             quick_sort(list_file, ["Name"])
-
         if order == 'asc':
-            return list_file[limit::-1]
-        print(list_file[:limit+1])
+            return list_file[limit-1::-1]
+        return list_file[:limit]
 
 
 
 
-select_sorted(filename='all_stocks_5yr.csv')
-#all_stocks_5yr.csv
+# print(select_sorted(filename='all_stocks_5yr.csv', order='desc', limit=3))
+#
+# print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3))
+#
+# print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3, group_by_name=True))
+
