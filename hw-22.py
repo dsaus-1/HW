1b02c2a5d9ab704dbf1cad0c454ba871eaa15c81
Dima Saushkin <saushkin.reg@gmail.com>
2022-12-01 14:05:11 +0300

    'Apdated func'

--- hw-22.py
+++ hw-22.py
@@ -45,22 +45,26 @@ def select_sorted(sort_columns=["high"], limit=30, group_by_name=False, order='d
 
     with open(filename, encoding='utf-8') as open_file:
         filename_reader = csv.reader(open_file)
+        count = 0
         for line in filename_reader:
+            if count == 0:
+                count += 1
+                continue
             list_file.append(line)
         numline = len(open_file.readlines())
         quick_sort(list_file, sort_columns)
         if group_by_name:
             quick_sort(list_file, ["Name"])
         if order == 'asc':
-            return list_file[limit-1::-1]
+            return list_file[:len(list_file)-limit-1:-1]
         return list_file[:limit]
 
 
 
 
-# print(select_sorted(filename='all_stocks_5yr.csv', order='desc', limit=3))
-#
-# print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3))
-#
-# print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3, group_by_name=True))
+print(select_sorted(filename='all_stocks_5yr.csv', order='desc', limit=3))
+
+print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3))
+
+print(select_sorted(filename='all_stocks_5yr.csv', order='asc',limit=3, group_by_name=True))
 
